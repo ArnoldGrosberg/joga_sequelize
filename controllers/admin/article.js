c053876cfa96ac55e57a805a58fdbac4b9b09628
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-02-21 10:21:54 +0200

    Delete article data, works thru Postman

--- controllers/admin/article.js
+++ controllers/admin/article.js
@@ -57,9 +57,21 @@ const updateArticle = (req, res) => {
 	})
 }
 
+const deleteArticle = (req, res) => {
+
+	models.Article.destroy({where: { id: req.params.id }} )
+	.then(article => {
+		return res.status(200).json({ message: 'Article is deleted' });
+	})
+	.catch (error => {
+		return res.status(500).send(error.message);
+	})
+}
+
 // export controller functions
 module.exports = {
 	createArticle,
-	updateArticle
+	updateArticle,
+	deleteArticle
 	
 };
