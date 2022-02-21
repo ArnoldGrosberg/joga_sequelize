551a10ca94560558c913251f79c6aa65b874ef7e
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-02-21 09:08:31 +0200

    Created new article

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const authorRouter = require('./routes/author');
 app.use('/', articleRouter);
 app.use('/article', articleRouter)
 app.use('/author', authorRouter)
+app.use('admin/article', articleRouter)
 
 // simple route 
 //app.get("/", (req, res) => {
