ebd29f925d9cfeb39c39e526b5ae2112af0d9de3
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-03-01 09:11:35 +0200

    Created API for author information by author name and id

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const apiArticleController = require('../controllers/api/article');
-
+const apiAuthorController = require('../controllers/api/author');
 
 router.get('/article', apiArticleController.getArticleBySlug);
+router.get('/author', apiAuthorController.getAuthorBySlug);
 
 module.exports = router;
